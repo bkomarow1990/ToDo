44ae5a2554957ad617f2f0e3ba0a4df1c9c5c87b
Gnid_ct84 <Gnid_ct84@student.itstep.org>
2021-12-15 20:45:05 +0200

    Add project files.

---
+++ ToDoSite/Controllers/ToDoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoSite.Models;
+
+namespace ToDoSite.Controllers
+{
+    public class ToDoController : Controller
+    {
+        public List<ToDoModel> Actions { get; set; }
+        public ToDoController()
+        {
+            Actions = new List<ToDoModel>() { new ToDoModel() { Title = "Kupit slona", DoneDate = DateTime.Now, ToDate = DateTime.Now.AddDays(33), FromDate = DateTime.Now - TimeSpan.FromDays(12) }, new ToDoModel() { Title = "Pokormit slona", DoneDate = DateTime.Now.AddDays(2), ToDate = DateTime.Now.AddDays(33), FromDate = DateTime.Now - TimeSpan.FromDays(12) }, new ToDoModel() { Title = "Pokormit slon2", DoneDate = DateTime.Now.AddDays(2), ToDate = DateTime.Now.AddDays(33), FromDate = DateTime.Now - TimeSpan.FromDays(12) } };
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(Actions);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ToDoModel model)
+        {
+            Actions.Add(model);
+            return View();
+        }
+    }
+}
---
+++ ToDoSite/Models/ToDoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoSite.Models
+{
+    public class ToDoModel
+    {
+        public string Title { get; set; }
+        [DisplayName("From Date")]
+        public DateTime FromDate { get; set; }
+        [DisplayName("Done Date")]
+        public DateTime? DoneDate { get; set; }
+        [DisplayName("To Date")]
+        public DateTime? ToDate { get; set; }
+        public bool IsDone { get => DoneDate != null; }
+    }
+}
